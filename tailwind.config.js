7c20cb728d2254319d6912942a1d97a4f570b3cf
Gabriel Gomes <gabrigomez15@gmail.com>
2021-06-09 11:01:46 -0300

    tailwind extends configs

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#CE1212',
+        secondary: {
+          100: '#EBA83A'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
